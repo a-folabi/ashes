d1987dd3015d561b616fbb7065dbf07e6697e3ce
Prenziv <prenziv@gmail.com>
2025-05-05 20:50:05 -0400

    MeadSOS LPF Code Update

--- example_python/LPF_MeadSOS.py
+++ example_python/LPF_MeadSOS.py
@@ -63,9 +63,9 @@ numStages = 5
 LPFIsland = Island(Top)
 
 Vin = Wire(Top)
-Vouts = [0]*5
-Vout_Bufs = [0]*5
-instances = [0]*5
+Vouts = [0]*numStages
+Vout_Bufs = [0]*numStages
+instances = [0]*numStages
 
 for i in range(numStages):  
 	MeadVin = None
@@ -80,20 +80,20 @@ Vout = Vouts[numStages-1]
 
 # FG Programming
 # -------------------------------------------------------------------------------
-drainBits = int(np.ceil(np.log2(2*numStages*4)))
-CAB_DrainDecoder = STD_DrainDecoder(Top,LPFIsland,bits=drainBits)
-CAB_DrainSelect = RunDrainSwitch(Top,LPFIsland,num=2*numStages)
-CAB_DrainSwitch = DrainCutoff(Top,LPFIsland,num=2*numStages)
+drainBits = int(np.ceil(np.log2(numStages*4)))
+DrainDecoder = STD_DrainDecoder(Top,LPFIsland,bits=drainBits)
+DrainSelect = RunDrainSwitch(Top,LPFIsland,num=numStages)
+DrainSwitch = DrainCutoff(Top,LPFIsland,num=numStages)
 
 # Connect program drains to drain switch
 for i in range(numStages):
-      CAB_DrainSwitch.PR[4*i] += instances[i][0].VD_P[0]
-      CAB_DrainSwitch.PR[(4*i)+1] += instances[i][0].VD_P[1]
+      DrainSwitch.PR[4*i] += instances[i][0].VD_P[0]
+      DrainSwitch.PR[(4*i)+1] += instances[i][0].VD_P[1]
       
-      CAB_DrainSwitch.PR[(4*i)+2] += instances[i][1].VD_P[0]
-      CAB_DrainSwitch.PR[(4*i)+3] += instances[i][1].VD_P[1]
+      DrainSwitch.PR[(4*i)+2] += instances[i][1].VD_P[0]
+      DrainSwitch.PR[(4*i)+3] += instances[i][1].VD_P[1]
     
-#GateDecoder = STD_IndirectGateDecoder(Top,LPFIsland,1)
+GateDecoder = STD_IndirectGateDecoder(Top,LPFIsland,2)
 GateSwitches0 = STD_IndirectGateSwitch(Top,LPFIsland,1)
 GateSwitches = STD_IndirectGateSwitch(Top,LPFIsland,1,col=0)
 
@@ -107,10 +107,70 @@ outerPins = frame(Top)
 outerPins.createPort("W","Vin",connection = Vin)
 outerPins.createPort("E","Vout",connection = Vout)
 
+PIN_Vout_Buf = outerPins.createPort("E","Vout_Buf",dimension=numStages)
+for i in range(numStages):
+      Vout_Bufs[i] += PIN_Vout_Buf[i]
+
+PROG = outerPins.createPort("N","Prog")
+RUN = outerPins.createPort("N","Prog")
+VGRUN = outerPins.createPort("N","VGRUN")
+VGPROG = outerPins.createPort("N","VGPROG")
+
+VTUN = outerPins.createPort("N","VTUN")
+AVDD = outerPins.createPort("N","AVDD")
+GND_N = outerPins.createPort("N","gnd")
+GND_S = outerPins.createPort("S","gnd")
+VINJ_N = outerPins.createPort("N","vinj")
+VINJ_S = outerPins.createPort("S","vinj")
+
+Drainline = outerPins.createPort("S","Drainline_Prog")
+
+GateEnable = outerPins.createPort("N","GateEnable")
+GateB = outerPins.createPort("W","GateB",dimension=2)
+
+DrainEnable = outerPins.createPort("W","DrainEnable")
+DrainB = outerPins.createPort("W","DrainB",dimension=drainBits)
+
+# Pin Connections
+# -------------------------------------------------------------------------------
+GateSwitches.RUN_IN += VGRUN[0]
+GateSwitches0.VINJ_T += VINJ_N
+GateSwitches0.GND_T += GND_N
+GateSwitches.Vgsel += VGPROG
+GateSwitches.PROG += PROG
+GateSwitches.RUN += RUN
+
+GateDecoder.VINJV += VINJ_N
+GateDecoder.GNDV += GND_N
+GateDecoder.ENABLE += GateEnable
+GateDecoder.IN += GateB
+
+DrainSwitch.VDD += VINJ_S
+DrainSwitch.GND += GND_S
+DrainSwitch.RUN += RUN
+
+DrainSelect.VINJ += VINJ_S
+DrainSelect.GND += GND_S
+DrainSelect.prog_drainrail += Drainline
+
+DrainDecoder.VINJ += VINJ_S
+DrainDecoder.GND += GND_S
+DrainDecoder.IN += DrainB
+DrainDecoder.ENABLE += DrainEnable
+
+
+instances[numStages-1][1].GND_b += GND_S
+instances[numStages-1][1].VINJ_b += VINJ_S
+instances[0][0].VINJ += VINJ_N
+instances[0][0].GND += GND_N
+instances[0][0].VTUN += VTUN
+instances[0][0].VPWR += AVDD[0]
+instances[0][0].PROG += PROG
+instances[0][0].RUN += RUN
+
 
 design_limits = [5e5, 5e5]
-location_islands = ((20000,1000),(0,2000))#<-location for tile v1
-#location_islands=None
+location_islands = ((50000,25000),(0,0))#<-location for tile v1
 
 
-compile_asic(Top,process="TSMC350nm",fileName="LPF_MeadSOS",p_and_r = True,design_limits = design_limits, location_islands = location_islands,drainSpaceIdx=0,drainSpace = 100,gateSpaceIdx=0,gateSpace=100)
+compile_asic(Top,process="TSMC350nm",fileName="LPF_MeadSOS",p_and_r = True,design_limits = design_limits, location_islands = location_islands,drainSpaceIdx=0,drainSpace = 15,gateSpaceIdx=0,gateSpace=10)
