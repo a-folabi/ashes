2764129168ee0d6a47b6f26c7b24899e5277e204
Pranav Mathews <39277680+Prenziv@users.noreply.github.com>
2025-05-18 15:44:55 -0400

    QDAC Pins and Connections

--- example_python/QDAC.py
+++ example_python/QDAC.py
@@ -13,13 +13,13 @@ import numpy as np
 
 def QDAC(circuit,numStages=1,QDACIsland=None,islandLoc = [0,0]):
     Top = circuit
-    QDACIsland = ac.Island(Top)
 
-    EPOTs = lib_dc.TSMC350nm_EPOT(Top,QDACIsland,dim=[numStages,1])
+    QDACIsland = ac.Island(Top)
+    EPOTs = lib_dc.TSMC350nm_EPOT(Top,QDACIsland,dim=[numStages+1,1])
     EPOTs.place([0,0])
 
     InvertingAmp = lib_dc.TSMC350nm_Amplifier9T_FGBias(Top,QDACIsland)
-    InvertingAmp.place([numStages+1,0])
+    InvertingAmp.place([numStages+2,0])
 
     TgateIsland0 = ac.Island(Top)
     SEL_Code = lib_dc.TSMC350nm_TGate_DT(Top,TgateIsland0,dim=[numStages,1])
@@ -47,12 +47,112 @@ def QDAC(circuit,numStages=1,QDACIsland=None,islandLoc = [0,0]):
     GateSwitches0 = lib_mux.STD_IndirectGateSwitch(Top,QDACIsland,1)
     GateSwitches = lib_mux.STD_IndirectGateSwitch(Top,QDACIsland,1,col=0)
 
-    drainLineNum = numStages*2+1
+    drainLineNum = (numStages+1)*2+1
     drainBits = int(np.ceil(np.log2(drainLineNum)))
 
     DrainDecoder = lib_mux.STD_DrainDecoder(Top,QDACIsland,bits=drainBits)
     DrainSelect = lib_mux.RunDrainSwitch(Top,QDACIsland,num=int(np.ceil(drainLineNum/4)))
     DrainSwitch = lib_cab.DrainCutoff(Top,QDACIsland,num=int(np.ceil(drainLineNum/4)))
+
+    for i in range(numStages+1):
+        DrainSwitch.PR[2*i] += EPOTs.VD_P[2*i]
+        DrainSwitch.PR[2*i+1] += EPOTs.VD_P[2*i+1]
+
+    DrainSwitch.PR[2*(numStages+1)] += InvertingAmp.VD_P
+    DrainSwitch.In[2*(numStages+1)] += InvertingAmp.VD_R 
+
+    # Pins
+    # -------------------------------------------------------------------------------
+    outerPins = lib_mux.frame(Top)
+
+    PROG = outerPins.createPort("N","Prog")
+    RUN = outerPins.createPort("N","Run")
+    VGRUN = outerPins.createPort("N","VGRUN")
+    VGPROG = outerPins.createPort("N","VGPROG")
+    VTUN = outerPins.createPort("N","VTUN")
+    AVDD = outerPins.createPort("N","AVDD")
+    GND_N = outerPins.createPort("N","gnd")
+    GND_S = outerPins.createPort("S","gnd")
+    VINJ_N = outerPins.createPort("N","vinj")
+    VINJ_S = outerPins.createPort("S","vinj")
+
+    DrainBits = outerPins.createPort("W","DrainB",dimension=drainBits)
+    DrainEnable = outerPins.createPort("W","DrainEnable")
+    GateBits = outerPins.createPort("W","GateB",dimension=2)
+    GateEnable = outerPins.createPort("W","GateEnable")
+    
+    VOUT = outerPins.createPort("S","Vout")
+    RST = outerPins.createPort("N","RST")
+    Code = outerPins.createPort("N","Code",dimension=numStages)
+
+    # Pin Connections
+    # -------------------------------------------------------------------------------
+    EPOTs.VDD += AVDD
+    EPOTs.VINJ += VINJ_N
+    EPOTs.VINJ_b += VINJ_S
+    EPOTs.GND += GND_N
+    EPOTs.GND_b += GND_S
+    EPOTs.Prog += PROG
+
+    Vref = ac.Wire(Top)
+    EPOTs.Vout[numStages-1] += Vref
+
+    EPOTs.Vout[0:numStages-2] += SEL_Code.A
+
+    SEL_Code.VDD += VINJ_N
+    SEL_Code.VDD_b += VINJ_S
+    SEL_Code.GND += GND_N
+    SEL_Code.GND_b += GND_S
+    SEL_Code.C += SEL_RST.B
+    SEL_Code.SELA += Code
+    SEL_Code.A += EPOTs.Vout[0:numStages]
+    SEL_Code.B += Vref
+
+    SEL_RST.VDD_b += VINJ_S
+    SEL_RST.GND += GND_N
+    SEL_RST.C += EPOTCap.Top
+    SEL_RST.SELA += RST[0]
+    SEL_RST.A += Vref
+
+    EPOTCap.Bot += InvertingAmp.VIN_PLUS[0]
+
+    Amp_RST.VDD_b += VINJ_S
+    Amp_RST.GND_b += GND_S
+    Amp_RST.SELA += RST[0]
+    Amp_RST.C += VOUT[0]
+    Amp_RST.A += Vref
+
+    InvertingAmp.VINJ += EPOTs.VINJ_b
+    InvertingAmp.VPWR += EPOTs.VDD_b
+    InvertingAmp.PROG += EPOTs.Prog_b
+    InvertingAmp.Vg += EPOTs.Vg_b[0]
+    InvertingAmp.Vsel += EPOTs.Vsel_b[0]
+    InvertingAmp.VIN_MINUS += Amp_RST.A
+    InvertingAmp.VIN_MINUS += CapFB.Top
+    InvertingAmp.Vout += CapFB.Bot
+    InvertingAmp.Vout += VOUT
+
+    GateSwitches.VINJ_T += VINJ_N[0]
+    GateSwitches.VINJ += EPOTs.VINJ
+    GateSwitches.PROG += PROG
+    GateSwitches.GND_T += GND_N[0]
+    GateSwitches.GND += GND_S[0]
+    GateSwitches.CTRL_B += EPOTs.Vg
+
+    DrainSwitch.RUN += RUN
+
+    DrainDecoder.VINJ += VINJ_S
+    DrainDecoder.GND += GND_S
+    DrainDecoder.IN += DrainBits
+    DrainDecoder.ENABLE += DrainEnable
+
+    GateDecoder.VINJV += VINJ_N
+    GateDecoder.GNDV += GND_N
+    GateDecoder.ENABLE += GateEnable
+    GateDecoder.IN += GateBits
+
+    # Island Placement
+    # -------------------------------------------------------------------------------
 
     EPOTWidth = 85000
     TGateWidth = 10000
@@ -69,15 +169,16 @@ def QDAC(circuit,numStages=1,QDACIsland=None,islandLoc = [0,0]):
 
     YEPOT = islandLoc[1]
     YIslands2 = YEPOT + 2*Pitch
+    Ybottom = YEPOT
 
-    location_islands = ((XEPOT,YEPOT),(XTGate0,YIslands2),(XTGate1,YIslands2),(XEPOTCap,YIslands2),(XAmpRST,YEPOT),(XCap,YEPOT))
+    location_islands = ((XEPOT,YEPOT),(XTGate0,YIslands2),(XTGate1,YIslands2),(XEPOTCap,YIslands2),(XAmpRST,Ybottom),(XCap,Ybottom))
 
     return location_islands
 
 
 Top = ac.Circuit()
 
-location_islands = QDAC(Top,5)
+location_islands = QDAC(Top,5,islandLoc=[50000,25000])
 
 design_limits = [5e5, 5e5]
 
--- example_verilog/QDAC.v
+++ example_verilog/QDAC.v
@@ -2,45 +2,48 @@ module TOP(port1);
 
 
 	/* Island 0 */
-	EPOT I__0 (.island_num(0), .row(0), .col(0), .matrix_row(5), .matrix_col(1));
-	TSMC350nm_Amplifier9T_FGBias I__1 (.island_num(0), .row(6), .col(0));
+	EPOT I__0 (.island_num(0), .row(0), .col(0), .matrix_row(6), .matrix_col(1), .VDDrow_0(net125[0:1]), .VDD_brow_5(net147[0:1]), .VINJrow_0(net164[0:1]), .VINJ_brow_5(net157[0:1]), .GNDrow_0(net165[0:1]), .GND_brow_5(net158[0:1]), .Progrow_0(net153[0:1]), .Prog_brow_5(net148[0:1]), .Vg_0_row_0(net154[0:1]), .Vg_1_row_0(net155[0:1]), .Vg_b_0_row_5(net149[0:1]), .Vsel_b_0_row_5(net150[0:1]), .VD_P_0_col_0(net108[0:6]), .VD_P_1_col_0(net109[0:6]), .Voutcol_0(net12[0:6]));
+	TSMC350nm_Amplifier9T_FGBias I__1 (.island_num(0), .row(7), .col(0), .VDD(net147[0]), .VINJ(net157[0]), .Vg(net149[0]), .VD_P(net120[0]), .VD_R(net121[0]), .Vsel(net150[0]), .PROG(net148[0]), .VIN_PLUS(net145[4]), .VIN_MINUS(net12[4]), .Vout(net152));
 
  	/*Programming Mux */ 
-	TSMC350nm_VinjDecode2to4_htile decoder(.island_num(0), .direction(horizontal), .bits(2));
+	TSMC350nm_VinjDecode2to4_htile decoder(.island_num(0), .direction(horizontal), .bits(2), .decode_n0_ENABLE(net166), .decode_n0_VINJV(net164[0]), .decode_n0_GNDV(net165[0]), .decode_n0_n0_IN_1_(net168), .decode_n0_n0_IN_0_(net167));
 	TSMC350nm_IndirectSwitches switch(.island_num(0), .direction(horizontal), .num(1));
-	TSMC350nm_IndirectSwitches switch_ind(.island_num(0), .direction(horizontal), .col(0));
-	TSMC350nm_VinjDecode2to4_vtile decoder(.island_num(0), .direction(vertical), .bits(4));
-	TSMC350nm_drainSelect_progrundrains switch(.island_num(0), .direction(vertical), .num(3), .type(drain_select));
-	TSMC350nm_4TGate_ST_draincutoff switch(.island_num(0), .direction(vertical), .num(3), .type(prog_switch));
+	TSMC350nm_IndirectSwitches switch_ind(.island_num(0), .direction(horizontal), .col(0), .GND_T(net165[0]), .VINJ_T(net164[0]), .GND(net158[0]), .CTRL_B_0_(net154[0]), .CTRL_B_1_(net155[0]), .VINJ(net164[0]), .PROG(net153[0]));
+	TSMC350nm_VinjDecode2to4_vtile decoder(.island_num(0), .direction(vertical), .bits(4), .decode_n0_VINJ(net157[0]), .decode_n0_GND(net158[0]), .decode_n0_IN_1_(net162), .decode_n0_IN_0_(net161), .decode_n2_IN_1_(net160), .decode_n2_IN_0_(net159), .decode_n0_ENABLE(net163));
+	TSMC350nm_drainSelect_progrundrains switch(.island_num(0), .direction(vertical), .num(4), .type(drain_select));
+	TSMC350nm_4TGate_ST_draincutoff switch(.island_num(0), .direction(vertical), .num(4), .type(prog_switch), .switch_n0_PR_0_(net108[0]), .switch_n0_PR_1_(net109[0]), .switch_n0_PR_2_(net108[1]), .switch_n0_PR_3_(net109[1]), .switch_n1_PR_0_(net108[2]), .switch_n1_PR_1_(net109[2]), .switch_n1_PR_2_(net108[3]), .switch_n1_PR_3_(net109[3]), .switch_n2_PR_0_(net108[4]), .switch_n2_PR_1_(net109[4]), .switch_n2_PR_2_(net108[5]), .switch_n2_PR_3_(net109[5]), .switch_n3_PR_0_(net120[0]), .switch_n3_In_0_(net121[0]), .switch_n0_RUN(net156));
 
 
 	/* Island 1 */
-	TGate_DT I__0 (.island_num(1), .row(0), .col(0), .matrix_row(5), .matrix_col(1));
+	TGate_DT I__0 (.island_num(1), .row(0), .col(0), .matrix_row(5), .matrix_col(1), .VDDrow_0(net164[0:1]), .VDD_brow_4(net157[0:1]), .GNDrow_0(net165[0:1]), .GND_brow_4(net158[0:1]), .SELAcol_0(net131[0:5]), .Ccol_0(net126[0:5]), .Acol_0(net12[0:5]), .Bcol_0(net12[0:5]));
 
  	/*Programming Mux */ 
 
 
 	/* Island 2 */
-	TGate_DT I__0 (.island_num(2), .row(0), .col(0), .matrix_row(5), .matrix_col(1));
+	TGate_DT I__0 (.island_num(2), .row(0), .col(0), .matrix_row(5), .matrix_col(1), .VDD_brow_4(net157[0:1]), .GNDrow_0(net165[0:1]), .SELAcol_0(net146[0:5]), .Ccol_0(net140[0:5]), .Acol_0(net12[0:5]), .Bcol_0(net126[0:5]));
 
  	/*Programming Mux */ 
 
 
 	/* Island 3 */
-	Capacitor_80ff I__0 (.island_num(3), .row(0), .col(0), .matrix_row(5), .matrix_col(1));
+	Capacitor_80ff I__0 (.island_num(3), .row(0), .col(0), .matrix_row(5), .matrix_col(1), .Topcol_0(net140[0:5]), .Botcol_0(net145[0:5]));
 
  	/*Programming Mux */ 
 
 
 	/* Island 4 */
-	TGate_DT I__0 (.island_num(4), .row(0), .col(0));
+	TGate_DT I__0 (.island_num(4), .row(0), .col(0), .VDD_b(net157[0]), .GND_b(net158[0]), .SELA(net146[4]), .C(net152), .A(net12[4]));
 
  	/*Programming Mux */ 
 
 
 	/* Island 5 */
-	Capacitor_80ff I__0 (.island_num(5), .row(0), .col(0));
+	Capacitor_80ff I__0 (.island_num(5), .row(0), .col(0), .Top(net12[4]), .Bot(net152));
 
  	/*Programming Mux */ 
 
+
+	/* Frame */ 
+	tile_analog_frame cab_frame(.pin_layer(METAL3), .N_n_Prog(net153[0]), .N_n_Run(net156), .N_n_AVDD(net125[0]), .N_n_gnd(net165[0]), .S_s_gnd(net158[0]), .N_n_vinj(net164[0]), .S_s_vinj(net157[0]), .W_w_DrainB_0_(net159), .W_w_DrainB_1_(net160), .W_w_DrainB_2_(net161), .W_w_DrainB_3_(net162), .W_w_DrainEnable(net163), .W_w_GateB_0_(net167), .W_w_GateB_1_(net168), .W_w_GateEnable(net166), .S_s_Vout(net152), .N_n_RST(net146[4]), .N_n_Code_0_(net131[0]), .N_n_Code_1_(net131[1]), .N_n_Code_2_(net131[2]), .N_n_Code_3_(net131[3]), .N_n_Code_4_(net131[4]));
  endmodule
